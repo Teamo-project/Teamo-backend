f559d27a9ea48ace23c0f07035b2935aa58bd23a
HyunJng <khjung1654@gmail.com>
2023-07-11 14:35:51 +0900

    first commit

---
+++ src/main/java/com/teamo/teamo/TeamoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamo.teamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamoBackendApplication.class, args);
+    }
+
+}
